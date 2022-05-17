eb56de376a0066da6561bcad354a3ba9bbfbadee
Girish Theja <girishtheja116@gmail.com>
2022-05-17 09:44:21 +0530

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,54 @@
+// Home.js
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+} from "react-native";
+import SearchBar from "../components/SearchBar";
+
+const Home = () => {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [fakeData, setFakeData] = useState();
+
+  // get data from the fake api endpoint
+  useEffect(() => {
+    const getData = async () => {
+      const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+      );
+      const data = await apiResponse.json();
+      setFakeData(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.root}>
+      {!clicked && <Text style={styles.title}>ENTER THE TOPIC NAME</Text>}
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+     
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: "100%",
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+});
